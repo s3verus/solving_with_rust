6e8a09685bae1a39f173008bfc2d96ea9c486334
Ali <en.severus@gmail.com>
2022-01-01 21:11:38 +0330

    first commit...

---
+++ close_opens/src/main.rs
@@ -0,0 +1,44 @@
+use std::io;
+
+fn parser(count: usize, line: &String) -> &String {
+    let mut rbracket: i32 = 0; // ()
+    let mut sbracket: i32 = 0; // []
+    let mut cbracket: i32 = 0; // {}
+    for i in 0..count-1 {
+        println!("char in index {} is: {}",i ,&line[i..i+1]);
+        if &line[i..i+1] == "[" {
+            sbracket += 1;
+            println!("s:{} c:{} r:{}", sbracket, cbracket, rbracket);
+            continue;
+        } else if &line[i..i+1] == "{" {
+            cbracket += 1;
+            println!("s:{} c:{} r:{}", sbracket, cbracket, rbracket);
+            continue;
+        } else if &line[i..i+1] == "(" {
+            rbracket += 1;
+            println!("s:{} c:{} r:{}", sbracket, cbracket, rbracket);
+            continue;
+        }
+    }
+    line
+}
+
+fn main() {
+    let mut line = String::new();
+    println!("enter string: ");
+    io::stdin()
+        .read_line(&mut line)
+        .expect("failed to read line!");
+    let temp = &line[..1];
+    //println!("first char you entered is: {}", temp);
+    if temp == "}" {
+        line = "{".to_owned() + &line;
+    } else if temp == "]" {
+        line = "[".to_owned() + &line;
+    } else if temp == ")" {
+        line = "(".to_owned() + &line;
+    }
+    let count = line.len();
+    line = parser(count, &line).to_string();
+    println!("edited string: {}", line);
+}
