fe745dcaf8298388d63577cfbc52352ef3c6985e
Ali <en.severus@gmail.com>
2022-01-01 23:31:17 +0330

    commit 2 :)

--- close_opens/src/main.rs
+++ close_opens/src/main.rs
@@ -1,23 +1,36 @@
 use std::io;
 
 fn parser(count: usize, line: &String) -> &String {
+    //let opens = ["[", "(", "{"];
+    //let closes = ["]", ")", "}"];
     let mut rbracket: i32 = 0; // ()
     let mut sbracket: i32 = 0; // []
     let mut cbracket: i32 = 0; // {}
-    for i in 0..count-1 {
-        println!("char in index {} is: {}",i ,&line[i..i+1]);
-        if &line[i..i+1] == "[" {
+    for i in 0..count - 1 {
+        println!("char in index {} is: {}", i, &line[i..i + 1]);
+        if &line[i..i + 1] == "[" {
             sbracket += 1;
             println!("s:{} c:{} r:{}", sbracket, cbracket, rbracket);
             continue;
-        } else if &line[i..i+1] == "{" {
+        } else if &line[i..i + 1] == "{" {
             cbracket += 1;
             println!("s:{} c:{} r:{}", sbracket, cbracket, rbracket);
             continue;
-        } else if &line[i..i+1] == "(" {
+        } else if &line[i..i + 1] == "(" {
             rbracket += 1;
             println!("s:{} c:{} r:{}", sbracket, cbracket, rbracket);
             continue;
+        } else if &line[i..i + 1] == "]" {
+            if &line[i - 1..i] == "[" {
+                sbracket -= 1;
+                println!("s:{} c:{} r:{}", sbracket, cbracket, rbracket);
+                continue;
+            //} else if &line[i - 1..i] in closes {
+                // we should check we i-2 ...
+            } else if &line[i - 1..i] == "{" || &line[i - 1..i] ==  "(" {
+                println!("error!, can't parse that string!");
+                break;
+            }
         }
     }
     line
