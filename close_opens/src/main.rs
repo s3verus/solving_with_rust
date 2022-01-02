d8ee9bdebcacf1039d342abfd67eba0af40e39f9
Ali <en.severus@gmail.com>
2022-01-02 11:21:58 +0330

    add some new bugs...

--- close_opens/src/main.rs
+++ close_opens/src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
+extern crate unicode_segmentation;
+use unicode_segmentation::UnicodeSegmentation;
 
-fn parser(count: usize, line: &String) -> &String {
+fn parser(count: usize, mut line: &String, mut lr_parsing: bool) -> &String {
     //let opens = ["[", "(", "{"];
     //let closes = ["]", ")", "}"];
     let mut rbracket: i32 = 0; // ()
@@ -25,19 +27,56 @@ fn parser(count: usize, line: &String) -> &String {
                 sbracket -= 1;
                 println!("s:{} c:{} r:{}", sbracket, cbracket, rbracket);
                 continue;
-            //} else if &line[i - 1..i] in closes {
-                // we should check we i-2 ...
-            } else if &line[i - 1..i] == "{" || &line[i - 1..i] ==  "(" {
-                println!("error!, can't parse that string!");
+            // } else if &line[i - 1..i] in closes {
+            // we should check we i-2 ...
+            } else if &line[i - 1..i] == "{" || &line[i - 1..i] == "(" {
+                println!("error!, can't parse this string!");
+                continue;
+            } // else if &line[i - 1..i] == ""
+        } else if &line[i..i + 1] == "}" {
+            if &line[i - 1..i] == "{" {
+                sbracket -= 1;
+                println!("s:{} c:{} r:{}", sbracket, cbracket, rbracket);
+                continue;
+            } else if &line[i - 1..i] == "[" || &line[i - 1..i] == "(" {
+                println!("error!, can't parse this string!");
+                break;
+            }
+        } else if &line[i..i + 1] == ")" {
+            if &line[i - 1..i] == "(" {
+                sbracket -= 1;
+                println!("s:{} c:{} r:{}", sbracket, cbracket, rbracket);
+                continue;
+            } else if &line[i - 1..i] == "[" || &line[i - 1..i] == "{" {
+                println!("error!, can't parse this string!");
                 break;
             }
         }
     }
-    line
+    if lr_parsing == true {
+        lr_parsing = false;
+        let enil: String = line
+        .graphemes(true)
+        .rev()
+        .collect();
+        line = parser(count, &enil, lr_parsing);
+        &line
+    } else {
+         let enil: String = line
+        // Split the string into an Iterator of &strs, where each element is an
+        // extended grapheme cluster.
+        .graphemes(true)
+        // Reverse the order of the grapheme iterator.
+        .rev()
+        // Collect all the chars into a new owned String.
+        .collect();
+        &enil
+    }
 }
 
 fn main() {
     let mut line = String::new();
+    let mut lr_parsing = true;
     println!("enter string: ");
     io::stdin()
         .read_line(&mut line)
@@ -52,6 +91,6 @@ fn main() {
         line = "(".to_owned() + &line;
     }
     let count = line.len();
-    line = parser(count, &line).to_string();
+    line = parser(count, &line, true).to_string();
     println!("edited string: {}", line);
 }
